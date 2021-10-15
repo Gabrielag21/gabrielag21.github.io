8792a82a758431af1590a262fc3d87775c30711f
Gabriela Gonzalez <ggonzalez9313@gmail.com>
2021-10-14 22:24:56 -0400

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Gabrielag21"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CHECK ME OUT!!!
         </a>
       </header>
     </div>
