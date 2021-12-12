4a4ca8460cff94b07d8d9c6777211d3512bfac61
Gabriela Gonzalez <ggonzalez9313@gmail.com>
2021-12-12 01:40:40 -0500

    stage 1 of home page layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './profile_pic.png';
+import banner from './banner.jfif';
 import './App.css';
 import NavBar from './components/NavBar';
 
@@ -6,23 +7,43 @@ function App() {
   return (
     
     <div className="App">
+      <div className="banner">Welcome!</div>
       <NavBar/>
       <header className="App-header">
 
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
 
-        <p>My name is Gabriela! Nice to e-meet you!</p>
+        <p>My name is Gabriela! Nice to e-meet you!</p> */}
 
-  <a
+
+ 
+  {/* <a
           className="App-link"
           href="https://github.com/Gabrielag21"
           target="_blank"
           rel="noopener noreferrer">
           Github Link!
-        </a>
+        </a> */}
 
       </header>
+
+      <div>
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <p>Hello! It is I, Gabriela!</p>
+
+      <p>Click through my website to get to know me and my passion of the STEM Field. </p>
+      <p>Learn more about me in the "About Me" tab, check out my recent projects in the "Recent Projects" tab, or check out the many ways you can contact me by clicking the "Contact me" tab!</p>
+      <p>Thanks for stopping by!</p>
+
     </div>
+
+    <footer className="footer">
+  <p>2021 Galaxy Productions, Inc.</p>
+</footer>
+
+
+</div>    
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,18 +4,17 @@ function NavBar() {
   
   return (
     
-    <div className="NavBar">
-       
+   
+       <nav>
     <ul>
       <li> <a href="#home"> Home </a> </li>
       <li> <a href="#about"> About Me </a> </li>
       <li> <a href="#projects"> Recent Projects </a> </li>
       <li> <a href="#contact">Contact  </a> </li>
     </ul>
-    
+    </nav>
       
-      
-    </div>
+  
   );
 }
 export default NavBar;
